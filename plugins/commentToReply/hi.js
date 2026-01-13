cea2f4c9200374b2446e339f1c63ccb5590bd0dd
Mohammad-nayan-2442 <apinpm8@gmail.com>
2026-01-13 19:01:18 +0600

    Update hi.js

--- plugins/commentToReply/hi.js
+++ plugins/commentToReply/hi.js
@@ -8,6 +8,33 @@ module.exports = {
       commentId,
       `@[${sender.id}] 👋 Hey! How can I help you? 😊`
     );
+
+    await nayan.sendMessage(sender.id, {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: `👋 Hi! ${value.from.name}\n\nThanks for your comment 💙\nYou can contact us using the options below 👇`,
+          buttons: [
+            {
+              type: "web_url",
+              url: "https://t.me/MOHAMMADNAYAN",
+              title: "📨 Telegram"
+            },
+            {
+              type: "web_url",
+              url: "https://wa.me/8801615298449",
+              title: "💬 WhatsApp"
+            },
+            {
+              type: "phone_number",
+              title: "📞 Call Now",
+              payload: "+8801615298449"
+            }
+          ]
+        }
+      }
+    });
     return true; // stop others
   }
 };
